32df207248a03a3bef1f20e9f732fa3e6c02845d
Muhammad-Rafay <rafy.soomro@gmail.com>
2022-01-16 01:30:14 +0500

    first

---
+++ learning_app/basic_app/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from basic_app import views
+
+app_name = "basic_app"
+
+urlpatterns = [
+    path("index/", views.index, name="index"),
+    path("register", views.register, name="register"),
+    path("user_login",views.user_login,name="user_login")
+
+
+]
